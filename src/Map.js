b2297d752d2c6a00b64e078885799f5d94de7885
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-04-06 22:59:47 +0300

    rendered default map

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-const Map = () => (
-  <div id="map" className="map">
-    map goes here
-  </div>
-);
+class Map extends Component {
+  render() {
+    return (
+      <div id="map" className="map">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBlaBL7hioLy7fI47zfwk9fiSKrMJtWZWI' }}
+          defaultCenter={[54.679408, 25.284144]}
+          defaultZoom={16}
+        />
+      </div>
+    );
+  }
+}
 
 export { Map };
