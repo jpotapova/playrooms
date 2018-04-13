56556add9625ea8d1cfdb3d0a4d52333e5e320c6
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-04-13 21:56:42 +0300

    back to list

--- src/Store.js
+++ src/Store.js
@@ -5,6 +5,7 @@ class Store extends Component {
   constructor(props) {
     super(props);
     this.toMap = this.toMap.bind(this);
+    this.backToList = this.backToList.bind(this);
     this.showStore = this.showStore.bind(this);
     this.getClass = this.getClass.bind(this);
   }
@@ -23,27 +24,32 @@ class Store extends Component {
     this.props.map.panTo({ lat: this.props.lat, lng: this.props.lng });
     this.props.toggleStores();
   }
+  backToList() {
+    this.props.backToList();
+  }
   showStore() {
     this.props.showStore(this.props.id);
   }
   render() {
     return (
-      <div className={this.getClass()} onClick={this.showStore}>
-        <h4 className="list-group-item-heading">{this.props.title}</h4>
-        <p className="list-group-item-text">
-          {this.props.area}, {this.props.address}
-        </p>
-        <p className="list-group-item-text">
-          <a href={this.props.weblink} target="_blank">
-            {this.props.web}
-          </a>
-        </p>
-        <p className="list-group-item-text">tel. {this.props.phone}</p>
+      <div className={this.getClass()}>
+        <div className="main-details" onClick={this.showStore}>
+          <h4 className="list-group-item-heading">{this.props.title}</h4>
+          <p className="list-group-item-text">
+            {this.props.area}, {this.props.address}
+          </p>
+          <p className="list-group-item-text">
+            <a href={this.props.weblink} target="_blank">
+              {this.props.web}
+            </a>
+          </p>
+          <p className="list-group-item-text">tel. {this.props.phone}</p>
+        </div>
         <div className={this.props.openStore === this.props.id ? 'more-details' : 'more-details hidden'}>
           <WorkingHours hours={this.props.hours} />
           <p className="text-right">
             &nbsp;
-            <button className="btn btn-info to-list" type="button">
+            <button className="btn btn-info to-list" type="button" onClick={this.backToList}>
               Back&nbsp;<span className="glyphicon glyphicon-list" />
             </button>
             &nbsp;&nbsp;
--- src/StoreList.js
+++ src/StoreList.js
@@ -5,12 +5,17 @@ class StoreList extends React.Component {
   constructor(props) {
     super(props);
     this.showStore = this.showStore.bind(this);
+    this.backToList = this.backToList.bind(this);
     this.state = {
       openStore: -1
     };
   }
   showStore(id) {
     this.setState({ openStore: id });
+  }
+  backToList() {
+    this.setState({ openStore: -1 });
+    console.log(this.state.openStore);
   }
   render() {
     return (
@@ -30,6 +35,7 @@ class StoreList extends React.Component {
             id={index}
             showStore={this.showStore}
             openStore={this.state.openStore}
+            backToList={this.backToList}
           />
         ))}
       </div>
