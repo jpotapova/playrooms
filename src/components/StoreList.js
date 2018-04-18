bbd69f7a8779730fbddfa0d0c677edf20b4a497e
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-04-18 18:01:53 +0300

    refactor

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Store } from './Store.js';
 
 class StoreList extends React.Component {
+  getClass(desktop, showStores) {
+    var className = '';
+    if (desktop) {
+      className = 'list-group';
+    } else {
+      className = showStores ? 'list-group animated slideInLeft' : 'list-group animated slideOutLeft';
+    }
+    return className;
+  }
   render() {
     return (
-      <div
-        className={
-          this.props.desktop
-            ? 'list-group'
-            : this.props.showStores ? 'list-group animated slideInLeft' : 'list-group animated slideOutLeft'
-        }
-      >
+      <div className={this.getClass(this.props.desktop, this.props.showStores)}>
         {this.props.stores.map((store, index) => (
           <Store
             {...store}
